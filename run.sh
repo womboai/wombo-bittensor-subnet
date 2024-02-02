ee4c1e7ba3307efc521dee45309458e1d18f9311
Ashley Wright <ashley@msrandom.net>
2024-02-02 21:16:32 +0300

    Remove detach logic

--- run.sh
+++ run.sh
@@ -5,13 +5,9 @@ set -e
 docker build -f $1.Dockerfile -t subnet:$1 .
 
 docker run \
-  --detach \
   --network="host" \
   --gpus all \
   --env-file .env \
   -v ~/.bittensor:/root/.bittensor/ \
   -v ~/.cache:/root/.cache/ \
-  --name $1 \
   subnet:$1 \
-
-docker attach $1
