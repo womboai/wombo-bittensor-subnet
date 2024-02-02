60c314337191dcd7cd828a4399f721c948a976d9
Ashley Wright <ashley@msrandom.net>
2024-02-02 21:16:12 +0300

    to(device) more?

--- base/base/validator.py
+++ base/base/validator.py
@@ -307,9 +307,9 @@ class BaseValidatorNeuron(BaseNeuron):
 
         # Compute forward pass rewards, assumes uids are mutually exclusive.
         # shape: [ metagraph.n ]
-        scattered_rewards: torch.FloatTensor = self.scores.scatter(
+        scattered_rewards: torch.FloatTensor = self.scores.to(self.device).scatter(
             0, torch.tensor(uids).to(self.device), rewards.to(self.device)
-        ).to(self.device)
+        )
         bt.logging.debug(f"Scattered rewards: {rewards}")
 
         # Update scores with rewards produced by this step.
