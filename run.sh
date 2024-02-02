0799b61fb3a95a36a2e2e1de7492214ccbe465ea
Ashley Wright <ashley@msrandom.net>
2024-02-02 21:09:07 +0300

    Detach and reattach

--- run.sh
+++ run.sh
@@ -5,9 +5,13 @@ set -e
 docker build -f $1.Dockerfile -t subnet:$1 .
 
 docker run \
+  --detach \
   --network="host" \
   --gpus all \
   --env-file .env \
   -v ~/.bittensor:/root/.bittensor/ \
   -v ~/.cache:/root/.cache/ \
-  subnet:$1
+  --name $1 \
+  subnet:$1 \
+
+docker attach $1
