c90ce3661c07577d461603e5a014e6eedb8dfe2d
Ashley Wright <ashley@msrandom.net>
2024-02-05 20:31:37 +0300

    Add pillow

--- run_client.sh
+++ run_client.sh
@@ -6,8 +6,6 @@ docker build -f client.Dockerfile -t subnet:client .
 
 docker run \
   --network="host" \
-  --gpus all \
   --env-file .env \
   -v ~/.bittensor:/root/.bittensor/ \
-  -v ~/.cache:/root/.cache/ \
   subnet:client \
