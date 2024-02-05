4b968a4d587476da1d5d3147604d5f03a8d6042d
Ashley Wright <ashley@msrandom.net>
2024-02-05 23:32:45 +0300

    Add simple validator run script

--- run_neuron.sh
+++ run_neuron.sh
@@ -7,8 +7,10 @@ docker build -f $1.Dockerfile -t subnet:$1 .
 
 docker run \
   --network="host" \
+  --detach \
   --gpus all \
   --env-file .env \
   -v ~/.bittensor:/root/.bittensor/ \
   -v ~/.cache:/root/.cache/ \
+  --name $1 \
   subnet:$1 \
