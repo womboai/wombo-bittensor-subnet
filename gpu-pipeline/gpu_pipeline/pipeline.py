88897f957ba676d9572f0bd142c949e9f8d4a690
Salman <salmanshahidzia@gmail.com>
2024-02-07 17:27:38 +0000

    add lora and correct checkpoint

--- gpu-pipeline/gpu_pipeline/pipeline.py
+++ gpu-pipeline/gpu_pipeline/pipeline.py
@@ -1,14 +1,46 @@
 from asyncio import Semaphore
+import os
+import requests
 from typing import Tuple
 
 from diffusers import StableDiffusionXLPipeline
 
 
-def get_pipeline() -> Tuple[Semaphore, StableDiffusionXLPipeline]:
-    pipeline = (
-        StableDiffusionXLPipeline
-        .from_pretrained("stablediffusionapi/newdream-sdxl-20")
-        .to("cuda")
+def ensure_file_at_path(path: str, url: str) -> None:
+    if not os.path.exists(path):
+        parent_dir = os.path.dirname(path)
+        if parent_dir:
+            os.makedirs(parent_dir, exist_ok=True)
+        print(f"Downloading {url} to {path}")
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            with open(path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    return path
+
+
+def get_model_path() -> str:
+    return ensure_file_at_path(
+        path="newdreamxl_v10.safetensors",
+        url="https://civitai.com/api/download/models/173961"
     )
 
-    return Semaphore(), pipeline
+
+def get_tau_lora_path() -> str:
+    return ensure_file_at_path(
+        path="tau_lora_v3_epoch3.safetensors",
+        url=None
+    )
+
+
+def get_pipeline() -> Tuple[Semaphore, StableDiffusionXLPipeline]:
+    pipe = (
+        StableDiffusionXLPipeline
+        .from_pretrained(get_model_path())
+        .to("cuda")
+    )
+    # TODO: uncomment once we're ready to use LORA
+    # pipe.load_lora_weights(get_tau_lora_path())
+    # pipe.fuse_lora()
+    return Semaphore(), pipe
