70f4d3eaa5c4b2a8c26bbad3827b5fb6d52beb65
Ashley Wright <ashley@msrandom.net>
2024-02-13 20:05:04 +0300

    Change target time

--- validator/validator/reward.py
+++ validator/validator/reward.py
@@ -35,7 +35,7 @@ async def reward(validation_endpoint: str, query: ImageGenerationInputs, synapse
     - float: The reward value for the miner.
     """
 
-    target_time = 0.09375
+    target_time = 1.0
     time_reward = target_time / synapse.dendrite.process_time
 
     async with ClientSession() as session:
