b45ce9c4810520e85e3e81f3a707a058e18dba0b
Ashley Wright <ashley@msrandom.net>
2024-02-13 03:48:40 +0300

    Consistent naming

--- image-generator/run.sh
+++ image-generator/run.sh
@@ -3,11 +3,11 @@
 set -e
 
 docker build -f ../gpu-pipeline/Dockerfile -t wombo-subnet:gpu-pipeline ../
-docker build -f Dockerfile -t wombo-subnet:image_generator ../
+docker build -f Dockerfile -t wombo-subnet:image-generator ../
 
 docker run \
   --network="host" \
   --gpus all \
   -v ~/.cache:/root/.cache/ \
   -i \
-  wombo-subnet:image_generator \
+  wombo-subnet:image-generator \
--- validator-api/run.sh
+++ validator-api/run.sh
@@ -3,11 +3,11 @@
 set -e
 
 docker build -f ../gpu-pipeline/Dockerfile -t wombo-subnet:gpu-pipeline ../
-docker build -f Dockerfile -t wombo-subnet:validator_api ../
+docker build -f Dockerfile -t wombo-subnet:validator-api ../
 
 docker run \
   --network="host" \
   --gpus=all \
   -v ~/.cache:/root/.cache/ \
   -i \
-  wombo-subnet:validator_api \
+  wombo-subnet:validator-api \
