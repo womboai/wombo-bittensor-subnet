d44d61a792d00c38ac939f46f49a14e7ea127852
Ashley Wright <ashley@msrandom.net>
2024-02-14 22:47:03 +0300

    Cleaner get_random_uids

--- tensor/tensor/uids.py
+++ tensor/tensor/uids.py
@@ -16,10 +16,8 @@ def get_random_uids(
     self,
     k: int,
     availability_checker: Callable[["bt.metagraph.Metagraph", int, int], bool],
-    exclude: List[int] = None,
 ) -> torch.LongTensor:
-    candidate_uids = []
-    avail_uids = []
+    available_uids = []
 
     for uid in range(self.metagraph.n.item()):
         uid_is_available = (
@@ -27,19 +25,8 @@ def get_random_uids(
             availability_checker(self.metagraph, uid, self.config.neuron.vpermit_tao_limit)
         )
 
-        uid_is_not_excluded = exclude is None or uid not in exclude
-
         if uid_is_available:
-            avail_uids.append(uid)
-            if uid_is_not_excluded:
-                candidate_uids.append(uid)
+            available_uids.append(uid)
 
-    # Check if candidate_uids contain enough for querying, if not grab all avaliable uids
-    available_uids = candidate_uids
-    if len(candidate_uids) < k:
-        available_uids += random.sample(
-            [uid for uid in avail_uids if uid not in candidate_uids],
-            k - len(candidate_uids),
-        )
-    uids = torch.tensor(random.sample(available_uids, k))
+    uids = torch.tensor(random.sample(available_uids, min(k, len(available_uids))))
     return uids
