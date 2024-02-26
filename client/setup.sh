0471b401a974f6c541e5f32172fdd927c75a6a4b
Ashley Wright <ashley@msrandom.net>
2024-02-26 23:27:24 +0300

    Fix torch index

--- client/setup.sh
+++ client/setup.sh
@@ -3,6 +3,6 @@
 python3 -m venv venv
 
 venv/bin/pip install -e ../image-generation-protocol
-venv/bin/pip install -e ../tensor
+venv/bin/pip install -e ../tensor --extra-index-url "https://download.pytorch.org/whl/cpu/"
 venv/bin/pip install -e ../neuron-selector
 venv/bin/pip install -e .
