5808d7303f5ef0a910bb108771d13588de762e09
Amrit Parmanand <amrit@wombo.ai>
2024-03-04 18:13:47 -0500

    new oldest_uid function using heapdict (#15)
    * new oldest_uid function using heapdict
    
    * shuffle all_uids for more fairness
    
    * use oldest_uids function in validator
    
    * move heap to validator object, start using it in save and load state
    
    * heap by hotkey instead of uid, double checker logic
    
    * add heapdict project requirement for neuron selector
    
    * fix for 'dictionary changed size during iteration'
    
    * logging
    
    * the cherry pick didn't cherry pick :((((

--- neuron-selector/neuron_selector/uids.py
+++ neuron-selector/neuron_selector/uids.py
@@ -1,13 +1,14 @@
+import heapdict
 import torch
 import random
 import bittensor
 from typing import List
+from datetime import datetime
+
 
 from tensor.protocol import NeuronInfoSynapse
 
-
 DEFAULT_NEURON_INFO = NeuronInfoSynapse()
-
 
 async def sync_neuron_info(self):
     uids = [
@@ -75,3 +76,70 @@ def get_random_uids(
 
     uids = torch.tensor(random.sample(available_uids, min(k, len(available_uids))))
     return uids
+
+
+def get_oldest_uids(
+    self,
+    k: int,
+    validators: bool,
+) -> torch.LongTensor:
+    if validators:
+        def validator_condition(uid: int, info: NeuronInfoSynapse) -> bool:
+            return info.is_validator and self.metagraph.validator_permit[uid]
+    else:
+        def validator_condition(_uid: int, info: NeuronInfoSynapse) -> bool:
+            return info.is_validator is False
+
+    all_uids_and_hotkeys_dict = {
+        self.metagraph.axons[uid].hotkey: uid
+        for uid in range(self.metagraph.n.item())
+        if self.metagraph.axons[uid].is_serving
+    }
+
+    hotkeys = list(all_uids_and_hotkeys_dict.keys())
+    random.shuffle(hotkeys)
+    shuffled_miner_dict = {hotkey: all_uids_and_hotkeys_dict[hotkey] for hotkey in hotkeys}
+    # if this is not randomized, every new validator will have the same mining order in their heap upon first launch,
+    # which would likely perpetuate the problem this function solves
+
+    infos = {
+        uid: self.neuron_info.get(uid, DEFAULT_NEURON_INFO)
+        for uid in shuffled_miner_dict.values()
+    }
+    invalid_miner_list = [
+        hotkey
+        for hotkey, uid in shuffled_miner_dict.items()
+        if not validator_condition(uid, infos[uid])
+    ]
+    for hotkey in invalid_miner_list:
+        shuffled_miner_dict.pop(hotkey)
+
+    for hotkey in shuffled_miner_dict.keys():
+        if hotkey not in self.miner_heap:
+            self.miner_heap[hotkey] = 0
+
+    disconnected_miner_list = [
+        hotkey
+        for hotkey in self.miner_heap.keys()
+        if hotkey not in shuffled_miner_dict.keys()
+    ]
+    for hotkey in disconnected_miner_list:
+        self.miner_heap.pop(hotkey)
+
+    bittensor.logging.info("Available miners: " + str(shuffled_miner_dict))
+    bittensor.logging.info("Miner heap: " + str(list(self.miner_heap.items())))
+    uids = torch.tensor(
+        [shuffled_miner_dict[hotkey] for hotkey in get_n_lowest_values(self.miner_heap, k)]
+    )
+    bittensor.logging.info("Selected miners: " + str(uids))
+    return uids
+
+
+def get_n_lowest_values(heap_dict: heapdict.heapdict, n):
+    lowest_values = []
+    for _ in range(min(n, len(heap_dict))):
+        hotkey, ts = heap_dict.popitem()
+        lowest_values.append(hotkey)
+        heap_dict[hotkey] = int(datetime.utcnow().timestamp())
+    bittensor.logging.info("Lowest values: " + str(lowest_values))
+    return lowest_values
