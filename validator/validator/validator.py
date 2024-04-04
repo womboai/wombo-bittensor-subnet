c991685a2f4f33c06053cd523dd8dd803479cf2a
Ashley Wright <ashley@msrandom.net>
2024-04-04 23:51:38 +0300

    Remove extra shuffling that's not needed

--- validator/validator/validator.py
+++ validator/validator/validator.py
@@ -209,37 +209,29 @@ class Validator(BaseNeuron):
             pending_validation_requests[0].get_loop().run_until_complete(asyncio.gather(*pending_validation_requests))
 
     def get_next_uid(self) -> tuple[int, str]:
-        all_uids_and_hotkeys_dict = {
+        miners = {
             self.metagraph.axons[uid].hotkey: uid
             for uid in range(self.metagraph.n.item())
             if self.metagraph.axons[uid].is_serving
         }
 
-        hotkeys = list(all_uids_and_hotkeys_dict.keys())
-
-        # if this is not randomized,
-        # every new validator will have the same mining order in their heap upon first launch,
-        # which would likely perpetuate the problem this function solves
-        random.shuffle(hotkeys)
-        shuffled_miner_dict = {hotkey: all_uids_and_hotkeys_dict[hotkey] for hotkey in hotkeys}
-
         infos = {
             uid: self.neuron_info.get(uid, DEFAULT_NEURON_INFO)
-            for uid in shuffled_miner_dict.values()
+            for uid in miners.values()
         }
 
         bt.logging.info(f"Neuron info found: {infos}")
 
         invalid_miner_list = [
             hotkey
-            for hotkey, uid in shuffled_miner_dict.items()
+            for hotkey, uid in miners.items()
             if infos[uid].is_validator is not False
         ]
 
         for hotkey in invalid_miner_list:
-            shuffled_miner_dict.pop(hotkey)
+            miners.pop(hotkey)
 
-        for hotkey in shuffled_miner_dict.keys():
+        for hotkey in miners.keys():
             # Push new miners to the start of the queue to give them a base score
             if hotkey not in self.miner_heap:
                 self.miner_heap[hotkey] = 0
@@ -247,7 +239,7 @@ class Validator(BaseNeuron):
         disconnected_miner_list = [
             hotkey
             for hotkey in self.miner_heap.keys()
-            if hotkey not in shuffled_miner_dict.keys()
+            if hotkey not in miners.keys()
         ]
 
         for hotkey in disconnected_miner_list:
@@ -267,7 +259,7 @@ class Validator(BaseNeuron):
 
         self.miner_heap.pop(hotkey)
 
-        return shuffled_miner_dict[hotkey], hotkey
+        return miners[hotkey], hotkey
 
     async def check_next_miner(self):
         """
