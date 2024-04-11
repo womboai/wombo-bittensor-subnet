ee238ed184682ddb92a65f0be7c24f2d006f7952
Ashley Wright <ashley@msrandom.net>
2024-04-11 20:46:50 +0300

    Remove test in the wrong place

--- image-generation-protocol/image_generation_protocol/cryptographic_sample.py
+++ image-generation-protocol/image_generation_protocol/cryptographic_sample.py
@@ -16,23 +16,6 @@
 #  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-#  The MIT License (MIT)
-#  Copyright © 2023 Yuma Rao
-#  Copyright © 2024 WOMBO
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-#  documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-#  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-#  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-#  the Software.
-#
-#  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-#  THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-#  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#  DEALINGS IN THE SOFTWARE.
 import os
 from functools import reduce
 from math import ceil, log
@@ -93,18 +76,3 @@ def cryptographic_sample(population: Sequence[T], k: int):
             result[i] = population[j]
 
     return result
-
-
-visited = set()
-
-i = 1
-while True:
-    if i == 1024:
-        break
-
-    for x in cryptographic_sample(range(1024), i):
-        visited.add(x)
-
-    print(list(visited))
-
-    i *= 2
