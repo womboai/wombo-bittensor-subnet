b29bbb19f232b95b2654e69d5d2cf125bc1040be
Ashley Wright <ashley@msrandom.net>
2024-04-17 00:46:44 +0300

    Off by one in the other direction?

--- validator-api/validator_api/validator_pipeline.py
+++ validator-api/validator_api/validator_pipeline.py
@@ -370,7 +370,7 @@ def __validate_internal(
         # Based on 3.4. in https://arxiv.org/pdf/2305.08891.pdf
         noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=pipeline.guidance_rescale)
 
-    for i in range(index + 1):
+    for i in range(index - 1):
         randn_tensor(
             latents.shape,
             generator=generator,
@@ -829,7 +829,7 @@ def __validate_internal_cn(
         noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
         noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
-    for i in range(index + 1):
+    for i in range(index - 1):
         randn_tensor(
             latents.shape,
             generator=generator,
